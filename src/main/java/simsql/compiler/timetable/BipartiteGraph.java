cd59b08483f239fba48a54e9f343efae40c16df0
dimitrijejankov <dimitrijejankov@gmail.com>
2017-05-09 16:31:46 -0500

    Added plan linking

--- src/main/java/simsql/compiler/timetable/BipartiteGraph.java
+++ src/main/java/simsql/compiler/timetable/BipartiteGraph.java
@@ -2,6 +2,8 @@ package simsql.compiler.timetable;
 
 import simsql.compiler.*;
 import simsql.compiler.expressions.MathExpression;
+import simsql.compiler.math_operators.EFunction;
+import simsql.compiler.math_operators.MathOperator;
 import simsql.compiler.operators.*;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -28,6 +30,30 @@ public class BipartiteGraph {
      * The the (table, set of dependent tables) pairs in general index form
      */
     private HashMap<String, HashSet<String>> backwardEdges;
+
+    /** Each random table (string) corresponds to an operator.
+     *  This map contains the instantiated random operators for the tables they represent.
+     */
+    private HashMap<String, Operator> generatedPlanMap = new HashMap<String, Operator>();
+
+    /** Each random table (string) has a list of tableScan which should be replaced,
+     *  because they should be linked with the rest of the query plan.
+     */
+    private HashMap<String, ArrayList<TableScan>> replacedPlanMap = new HashMap<String, ArrayList<TableScan>>();
+
+    /** list of tables in order they are removed for the graph
+     */
+    private ArrayList<String> tpList = new ArrayList<String>();
+
+    /**
+     * TODO for some reason this exists...
+     */
+    private ArrayList<Operator> hostOperatorList = new ArrayList<Operator>();
+
+    /**
+     * The operators we have linked...
+     */
+    private ArrayList<Operator> linkedOperatorList = new ArrayList<Operator>();
 
     /**
      * An instance of the TranslatorHelper class
@@ -117,6 +143,11 @@ public class BipartiteGraph {
         return retValue;
     }
 
+    /**
+     * Finds the backward edges for a given table
+     * @param node the table to find the backward edges for
+     * @return the set of backward edges
+     */
     private HashSet<String> findBackwardEdge(TimeTableNode node) {
         String tableName = node.getBracketsTableName();
         String tablePrefix = MultidimensionalTableSchema.getPrefixFromBracketsTableName(tableName);
@@ -145,14 +176,20 @@ public class BipartiteGraph {
         throw new RuntimeException("Failed to find an appropriate edge.");
     }
 
+    /**
+     * Generates the bipartite graph used for graph cutting optimization
+     * @throws Exception if the root of any random table query is not a projection!
+     */
     private void generateBipartiteGraph() throws Exception {
 
-        // Each random table (string) corresponds to an operator.
-        HashMap<String, Operator> generatedPlanMap = new HashMap<String, Operator>();
+        // makes an operator instance for every table in the table graph (generate partial plans...)
+        instantiateAllOperators();
 
-        // Each random table (string) has a list of tableScan which should be replaced.
-        HashMap<String, ArrayList<TableScan>> replacedPlanMap = new HashMap<String, ArrayList<TableScan>>();
+        // link the unconnected graphs
+        linkPartialPlans();
+    }
 
+    private void instantiateAllOperators() throws Exception {
         // while we haven't used up all the tables to instantiate their operators
         while(!this.nodes.isEmpty()) {
 
@@ -161,6 +198,9 @@ public class BipartiteGraph {
 
             // grab the qualified table name
             String table = MultidimensionalTableSchema.getQualifiedTableNameFromBracketsTableName(node.getBracketsTableName());
+
+            // add the table to the list
+            tpList.add(table);
 
             Operator operator;
 
@@ -174,13 +214,218 @@ public class BipartiteGraph {
             }
 
             CopyHelper copyHelper = new CopyHelper();
+
+            // copy the operator
             operator = operator.copy(copyHelper);
 
             HashMap<String, Integer> indices = MultidimensionalTableSchema.getIndicesFromBracketsName(table);
+
+            // make the random table operator a concrete operator for the given table with the given indices...
             instantiateOperator(table, operator, indices, generatedPlanMap, replacedPlanMap);
 
             // remove node from graph
             updateTableGraph(node);
+        }
+    }
+
+    private void linkPartialPlans() throws Exception {
+
+        // go through each table from back to front (the most dependent one to the least dependent one)
+        for (int index = tpList.size() - 1; index >= 0; index--) {
+
+            // grab the table name
+            String table = tpList.get(index);
+
+            // the operator for this table
+            Operator hostOperator = generatedPlanMap.get(table);
+
+            // store the host operator if we haven't done so already
+            if (!hostOperatorList.contains(hostOperator)) {
+                hostOperatorList.add(hostOperator);
+            }
+
+            // get all the tables that were replaced for this operator
+            ArrayList<TableScan> replacedTableList = replacedPlanMap.get(table);
+
+            // if there are tables that were replaced
+            if (replacedTableList != null) {
+
+                // go through each of them
+                for (TableScan tableScan : replacedTableList) {
+
+                    // extract their table name
+                    String tableName = tableScan.getTableName();
+
+                    // grab it from the instantiated operators
+                    Operator linkedOperator = generatedPlanMap.get(tableName);
+
+                    // integrate them together
+                    integratePlan(tableScan, linkedOperator);
+
+                    // add the linked operator to the linked operators if its not already there
+                    if (!linkedOperatorList.contains(linkedOperator)) {
+                        linkedOperatorList.add(linkedOperator);
+                    }
+                }
+            }
+        }
+    }
+
+    private void integratePlan(TableScan tableScan, Operator linkedOperator) throws Exception {
+
+        // grab the attributes from the table scan...
+        ArrayList<String> new_attributeList = tableScan.getAttributeList();
+        Operator newOperator;
+
+        // if the instance is a projection (it must be)
+        if (linkedOperator instanceof Projection) {
+
+            // list of old attributes of the projection (they are currently set)
+            ArrayList<String> old_attributeList = ((Projection) linkedOperator).getProjectedNameList();
+
+            // we change their name to match the attributes from the table scan
+            newOperator = changeAttributeName(linkedOperator, old_attributeList, new_attributeList);
+
+            // go through all the parents that depend on this table scan
+            ArrayList<Operator> parentList = tableScan.getParents();
+            for (Operator aParentList : parentList) {
+
+                // add it as a parent of the new operator
+                newOperator.addParent(aParentList);
+
+                // replace the table scan in the list of children with the new operator
+                aParentList.replaceChild(tableScan, newOperator);
+            }
+
+            // clear the links of the table scan so we don't bother with it anymore
+            tableScan.clearLinks();
+        } else {
+            throw new Exception("The root of an random table plan is not projection!");
+        }
+    }
+
+
+    private Operator changeAttributeName(Operator originalElement,
+                                         ArrayList<String> old_attributeNameList,
+                                         ArrayList<String> new_attributeNameList) throws Exception {
+        if (old_attributeNameList == null ||
+                new_attributeNameList == null ||
+                old_attributeNameList.size() != new_attributeNameList.size()) {
+            throw new Exception("change attribute error!");
+        }
+		/*
+		 * Scalar function followed by projection
+		 */
+        if (old_attributeNameList.size() != 0) {
+			/*
+			 * 1. Scalar function on the new defined attribute due to the
+			 * definition of the schema.
+			 */
+
+			/*
+			 * The data structure in the ScalarFunction node.
+			 */
+            String nodeName = "node" + translatorHelper.getInstantiateNodeIndex();
+            ArrayList<Operator> children = new ArrayList<Operator>();
+            ArrayList<Operator> parents = new ArrayList<Operator>();
+            ArrayList<MathOperator> scalarExpressionList = new ArrayList<MathOperator>();
+            HashMap<MathOperator, ArrayList<String>> columnListMap = new HashMap<MathOperator, ArrayList<String>>();
+            HashMap<MathOperator, String> outputMap = new HashMap<MathOperator, String>();
+
+			/*
+			 * 1.1. Fill the translatedStatement in the ScalarFunction.
+			 */
+            for (int i = 0; i < old_attributeNameList.size(); i++) {
+				/*
+				 * 1.2. Fill in the scalarFunction with the concrete
+				 * MathFunction Here it should be EFunction.
+				 */
+                scalarExpressionList.add(new EFunction());
+            }
+
+			/*
+			 * It comes to the attribute set of each function. Since one scalar
+			 * function can have multiple functions, with each function can have
+			 * multiple involved attributes. However, since the scalar function
+			 * only plays the role of renaming, each scalar function has only
+			 * one attribute.
+			 */
+
+            ArrayList<String> tempList;
+            for (int i = 0; i < old_attributeNameList.size(); i++) {
+				/*
+				 * 1.3. Fill each functions in the ScalarFunction with involved
+				 * attributes.
+				 */
+                tempList = new ArrayList<String>();
+                tempList.add(old_attributeNameList.get(i));
+                columnListMap.put(scalarExpressionList.get(i), tempList);
+            }
+
+            for (int i = 0; i < new_attributeNameList.size(); i++) {
+				/*
+				 * 1.4. Fill each functions in the ScalarFunction with an output
+				 */
+                outputMap.put(scalarExpressionList.get(i), new_attributeNameList.get(i));
+            }
+
+			/*
+			 * 1.5. Fill in the children
+			 */
+            children.add(originalElement);
+
+			/*
+			 * 1.6 Create the current scalar function node.
+			 */
+            ScalarFunction scalarFunction = new ScalarFunction(nodeName, children, parents, translatorHelper);
+            scalarFunction.setScalarExpressionList(scalarExpressionList);
+            scalarFunction.setColumnListMap(columnListMap);
+            scalarFunction.setOutputMap(outputMap);
+
+			/*
+			 * 1.7 This translatedElement add current Node as parent
+			 */
+            originalElement.addParent(scalarFunction);
+
+			/*
+			 * 2. Projection on the result attribute
+			 */
+			/*
+			 * 2.1 Create the data structure of the Projection
+			 */
+            Projection projection;
+            nodeName = "node" + translatorHelper.getInstantiateNodeIndex();
+            children = new ArrayList<Operator>();
+            parents = new ArrayList<Operator>();
+            ArrayList<String> projectedNameList = new ArrayList<String>();
+
+			/*
+			 * 2.2 Fill the tranlsatedResult.
+			 */
+            for (String aNew_attributeNameList : new_attributeNameList) {
+                /*
+				 * 2.3 Fill the projectedNameList
+				 */
+                projectedNameList.add(aNew_attributeNameList);
+            }
+
+			/*
+			 * 2.4 Fill the children
+			 */
+            children.add(scalarFunction);
+
+			/*
+			 * 2.5 Create the current projection node.
+			 */
+            projection = new Projection(nodeName, children, parents,
+                    projectedNameList);
+			/*
+			 * 2.6 "sclarFunction" fills it parents with the projection.
+			 */
+            scalarFunction.addParent(projection);
+            return projection;
+        } else {
+            return originalElement;
         }
     }
 
